f9bdb762a917f03e8c90b56ed864515b8da67818
Antonio <wapasmith@gmail.com>
2021-01-22 19:25:08 -0500

    Placed Planets and added Basic Logic
    + Solar bodies named and placed in scene
    + Rotation logic implemented
    + Basic Revolution logic implemented
    + Camera focus logic implemented
    All variables are able to be changed within the Inspector

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public string population;
+    public Transform target;
+
+    // [Range(-10, 10)] public float revolutionSpeed = 0; // Allows a range of speed for the planets between -10 and 10
+
+    [Range(-100, 100)] public float Xradius; 
+    [Range(-100, 100)] public float Yradius; // Separated radius into each axes so it's editable in Inspector
+    [Range(-100, 100)] public float Zradius;
+    public float age;
+    
+
+    void Update()
+    {
+        RevolveAround();
+    }
+
+    public void RevolveAround()
+    {
+        age += Time.deltaTime;
+
+        /* F A I L E D  A T T E M P T  1
+        //Vector3 offset = new Vector3();
+        //offset.x = revolutionSpeed * (Mathf.Sin(age) * radius);
+        //// offset.y = revolutionSpeed * (Mathf.Cos(age) * radius); // no need for revolution on y-axis right now
+        //offset.z = revolutionSpeed * (Mathf.Tan(age) * radius);
+
+        //transform.position = target.position + offset;
+        //transform.position = new Vector2(offset.x, offset.z) * target.position;
+        */
+
+        /* F A I L E D  A T T E M P T  2
+        //Vector3 offset = new Vector3();
+        //offset.x = Mathf.Sin(age) * 200 + radius / 2;
+        //offset.y = Mathf.Cos(age) * 200 + radius / 2;
+        //offset.z = Mathf.Tan(age) * 200 + radius / 2;
+
+        //transform.position = target.position + offset;
+        */
+        
+        Vector3 offset = new Vector3();
+        offset.x = Mathf.Sin(age) * Xradius;
+        offset.y = Mathf.Tan(age) * Yradius;
+        offset.z = Mathf.Cos(age) * Zradius;
+
+        transform.position = target.position + offset;
+    }
+
+}
