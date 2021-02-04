9a69f095ad58bfea5e20871bcc2b7393c3835e4e
Antonio <wapasmith@gmail.com>
2021-02-04 00:04:49 -0500

    Planet Selector Completed and Code Refactoring
    + Planets can be selected which will tell the camera to follow the selected planet
    - Removed LookAt.cs as it is now being handled by PlanetPicker.cs
    - Reomoved StateMachine.cs as it was never actually needed

--- Assets/Scripts/Buttons/PlanetPicker.cs
+++ Assets/Scripts/Buttons/PlanetPicker.cs
@@ -7,11 +7,49 @@ public class PlanetPicker : MonoBehaviour
 {
     public Camera cam;
     public Transform target;
+    public GameObject Solos;
+    public GameObject Terros;
+    public GameObject Lymphos;
+    public GameObject Tempos;
+    public GameObject Glados;
+    public GameObject Luminos;
+    public GameObject Phyros;
+
+    private void Update()
+    {
+        cam.transform.LookAt(target);
+    }
 
     public void FollowSolos()
     {
-        print("Solos pressed");
-        GetComponent<StateMachine>().followingSolos = true;
-        GetComponent<LookAt>().target = target;
+        target = Solos.transform;
+    }
+    public void FollowTerros()
+    {
+        target = Terros.transform;
+    }
+    public void FollowPhyros()
+    {
+        target = Phyros.transform;
+    }
+    public void FollowLymphos()
+    {
+        target = Lymphos.transform;
+    }
+    public void FollowTempos()
+    {
+        target = Tempos.transform;
+    }
+    public void FollowGlados()
+    {
+        target = Glados.transform;
+    }
+    public void FollowLuminos()
+    {
+        target = Luminos.transform;
+    }
+    public void FollowNone()
+    {
+        target = null;
     }
 }
